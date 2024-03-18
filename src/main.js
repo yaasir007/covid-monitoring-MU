2303a2dad7dc1809588f75136f323e7e16536988
yaasir cheekoory <yaasir1997@gmail.com>
2024-03-19 02:09:26 +0400

    install primevue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-light-green/theme.css'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.mount('#app')
